304fceb6238a266ceb62e67d82bd075bb73ad944
Jörg Thalheim <joerg@thalheim.io>
2017-04-19 22:32:58 +0200

    anbox: improve attribute names

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -12960,7 +12960,7 @@ with pkgs;
 
     amdgpu-pro = callPackage ../os-specific/linux/amdgpu-pro { };
 
-    anbox = callPackage ../applications/virtualization/anbox { };
+    inherit (callPackage ../applications/virtualization/anbox { }) ashmem binder;
 
     batman_adv = callPackage ../os-specific/linux/batman-adv {};
 
@@ -14153,6 +14153,8 @@ with pkgs;
   androidStudioPackages = callPackage ../applications/editors/android-studio { };
   android-studio = androidStudioPackages.stable;
   android-studio-preview = androidStudioPackages.preview;
+
+  inherit (callPackage ../applications/virtualization/anbox { }) anbox anbox-image;
 
   anbox = callPackage ../applications/virtualization/anbox { };
 
